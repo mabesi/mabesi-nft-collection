859de324552c14f3e4b359441db64c62ddd2b4eb
Plinio Mabesi <pliniomabesi@gmail.com>
2023-07-23 14:12:16 -0300

    OpenZeppelin imports

--- contracts/MabesiNFT.sol
+++ contracts/MabesiNFT.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+// OpenZeppelin Libraries ===========================
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/security/Pausable.sol";
+
 contract MabesiNFT {
 
 }
